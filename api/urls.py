fbc3efbb6f120d0c643d55750e7bd08d54b753b7
Luis Casas <luisscasas45@gmail.com>
2025-03-10 23:42:00 -0600

    FEAT/add token and token/refresh/ urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include  
 from rest_framework import routers
 from . import viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter() 
 router.register(r'owners', viewsets.OwnerViewSet)
@@ -8,4 +9,6 @@ router.register(r'species', viewsets.SpeciesViewSet)
 
 urlpatterns = [ 
     path("", include(router.urls)),  
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),  # <--- CORREGIDO
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
